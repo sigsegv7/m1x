61a2c7ec60942fd086a5dec36dd53401afb6dac2
Ian Moffett <ian@mirocom.org>
2026-03-25 08:46:13 -0400

    sdk: Add sys/units.h
    Signed-off-by: Ian Moffett <ian@mirocom.org>

---
+++ src/sdk/include/sys/units.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2026, Mirocom Laboratories
+ * Provided under the BSD-3 clause
+ */
+
+#ifndef _SYS_UNITS_H_
+#define _SYS_UNITS_H_ 1
+
+/* Storage size units */
+#define UNIT_GIB 0x40000000
+#define UNIT_MIB 0x100000
+
+#endif  /* !_SYS_UNITS_H_ */
