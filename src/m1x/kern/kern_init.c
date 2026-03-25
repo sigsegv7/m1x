8b5c04ecadbb2e8fc26befbf9d09777191f14e01
Ian Moffett <ian@mirocom.org>
2026-03-25 07:59:47 -0400

    m1x: Add minimal boot banner
    Signed-off-by: Ian Moffett <ian@mirocom.org>

--- src/m1x/kern/kern_init.c
+++ src/m1x/kern/kern_init.c
@@ -4,13 +4,33 @@
  */
 
 #include <dev/cons/cons.h>
+#include <lib/printf.h>
 #include <hal/cpu.h>
+
+/* Kernel version */
+#define _M1X_VERSION "0.0.1"
+
+/*
+ * Display a boot banner on early start up
+ */
+static void
+banner(void)
+{
+    printf(
+        "booting m1x/%s v%s...\n",
+        _M1X_ARCH,
+        _M1X_VERSION
+    );
+}
 
 void
 kmain(void)
 {
     /* Initialize the console */
     cons_init();
+
+    /* Print the boot banner */
+    banner();
 
     /* Initialize the BSP */
     hal_cpu_init();
