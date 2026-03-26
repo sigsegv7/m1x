04fb822f8e28601cf5a43446c0f806c0f6cf2ffc
Ian Moffett <ian@mirocom.org>
2026-03-26 02:33:20 -0400

    m1x/x86_64: Add initial trap code defs
    Signed-off-by: Ian Moffett <ian@mirocom.org>

---
+++ src/m1x/include/arch/x86_64/trap.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2026, Mirocom Laboratories
+ * Provided under the BSD-3 clause
+ */
+
+#ifndef _MACHINE_TRAP_H_
+#define _MACHINE_TRAP_H_ 1
+
+#define TRAP_DIVERR  0x00       /* Divide error */
+#define TRAP_UD      0x01       /* Invalid opcode */
+#define TRAP_DF      0x02       /* Double fault */
+#define TRAP_TS      0x03       /* Invalid TSS */
+#define TRAP_NP      0x04       /* Segment not present */
+#define TRAP_SS      0x05       /* Stack segment fault */
+#define TRAP_GP      0x06       /* General protection fault */
+#define TRAP_PF      0x07       /* Page fault */
+
+#endif  /* !_MACHINE_TRAP_H_ */
