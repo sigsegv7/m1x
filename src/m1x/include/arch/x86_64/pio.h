43b17b5c084e1ea9ef4b9869e06684180987782a
Ian Moffett <ian@mirocom.org>
2026-03-27 00:13:36 -0400

    m1x/x86_64: Add port I/O helpers
    Signed-off-by: Ian Moffett <ian@mirocom.org>

---
+++ src/m1x/include/arch/x86_64/pio.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2026, Mirocom Laboratories
+ * Provided under the BSD-3 clause
+ */
+
+#ifndef _MACHINE_PIO_H_
+#define _MACHINE_PIO_H_ 1
+
+#include <sys/cdefs.h>
+#include <sys/types.h>
+
+static inline uint8_t
+md_inb(uint16_t port)
+{
+    uint8_t result;
+    __asmv("in %%dx, %%al" : "=a" (result) : "d" (port));
+    return result;
+}
+
+static inline uint16_t
+md_inw(uint16_t port)
+{
+    uint16_t val;
+    __asmv("inw %w1, %w0" : "=a" (val) : "Nd" (port));
+    return val;
+}
+
+static inline uint32_t
+md_inl(uint16_t port)
+{
+    uint32_t val;
+    __asmv("inl %w1, %0" : "=a" (val) : "Nd" (port));
+    return val;
+}
+
+static inline void
+md_outb(uint16_t port, uint8_t val)
+{
+    __asmv("out %%al, %%dx" : :"a" (val), "d" (port));
+}
+
+static inline void
+md_outw(uint16_t port, uint16_t val)
+{
+    __asmv("outw %w0, %w1" : : "a" (val), "Nd" (port));
+}
+
+static inline void
+md_outl(uint16_t port, uint32_t val)
+{
+    __asmv("outl %0, %w1" : : "a" (val), "Nd" (port));
+}
+
+#endif  /* !_MACHINE_PIO_H_ */
