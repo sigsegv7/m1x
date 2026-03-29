4a34d6ba41e2abc90e5c81391a5e175fbdcced3f
Ian Moffett <ian@mirocom.org>
2026-03-28 21:41:44 -0400

    m1x/x86_64: pmap: Add page table flags
    Signed-off-by: Ian Moffett <ian@mirocom.org>

--- src/m1x/arch/x86_64/cpu/cpu_pmap.c
+++ src/m1x/arch/x86_64/cpu/cpu_pmap.c
@@ -6,6 +6,23 @@
 #include <sys/cdefs.h>
 #include <hal/pmap.h>
 
+/*
+ * Page-Table Entry (PTE) flags
+ *
+ * See Intel SDM Vol 3A, Section 4.5, Table 4-19
+ */
+#define PTE_ADDR_MASK   0x000FFFFFFFFFF000
+#define PTE_P           BIT(0)        /* Present */
+#define PTE_RW          BIT(1)        /* Writable */
+#define PTE_US          BIT(2)        /* User r/w allowed */
+#define PTE_PWT         BIT(3)        /* Page-level write-through */
+#define PTE_PCD         BIT(4)        /* Page-level cache disable */
+#define PTE_ACC         BIT(5)        /* Accessed */
+#define PTE_DIRTY       BIT(6)        /* Dirty (written-to page) */
+#define PTE_PS          BIT(7)        /* Page size */
+#define PTE_GLOBAL      BIT(8)        /* Global / sticky */
+#define PTE_NX          BIT(63)       /* Execute-disable */
+
 void
 hal_read_vas(struct vas *res)
 {
