2af8c2b0dc5f1fd258397771c93e322cb70ed412
Ian Moffett <ian@mirocom.org>
2026-03-29 19:01:34 -0400

    m1x/x86_64: irqchip: Keep track of I/O + Local APICs
    Signed-off-by: Ian Moffett <ian@mirocom.org>

--- src/m1x/arch/x86_64/dev/irqchip.c
+++ src/m1x/arch/x86_64/dev/irqchip.c
@@ -18,10 +18,73 @@
 /* Presence of i8259s */
 #define MADT_PCAT_COMPAT  BIT(0)
 
+/* For keeping track of IRQ chips */
+static struct irqchip ioapic_list[IRQCHIP_MAX];
+static struct irqchip lapic_list[IRQCHIP_MAX];
+static size_t ioapic_count = 0;
+static size_t lapic_count = 0;
+
+static inline void
+ioapic_add(struct irqchip *chip)
+{
+    if (ioapic_count >= IRQCHIP_MAX) {
+        return;
+    }
+
+    ioapic_list[ioapic_count++] = *chip;
+    pr_trace(
+        "ioapic @ mainbus : [id=%x, gsi_base=%x, base=%x]\n",
+        chip->id,
+        chip->gsi_base,
+        chip->base_addr
+    );
+}
+
+static inline void
+lapic_add(struct irqchip *chip, uint8_t processor_id)
+{
+    if (lapic_count >= IRQCHIP_MAX) {
+        return;
+    }
+
+    lapic_list[lapic_count++] = *chip;
+    pr_trace(
+        "lapic @ core%d : [id=%x, base=%x]\n",
+        processor_id,
+        chip->id,
+        chip->base_addr
+    );
+}
+
+struct irqchip *
+md_ioapic_index(size_t index)
+{
+    if (index >= ioapic_count) {
+        return NULL;
+    }
+
+    return &ioapic_list[index];
+}
+
+struct irqchip *
+md_lapic_index(size_t index)
+{
+    if (index >= lapic_count) {
+        return NULL;
+    }
+
+    return &lapic_list[index];
+}
+
 int
 hal_irqchip_init(void)
 {
     struct acpi_madt *madt;
+    struct local_apic *lapic;
+    struct ioapic *ioapic;
+    struct apic_header *apichdr;
+    struct irqchip chip;
+    uint8_t *cur, *end;
 
     if ((madt = acpi_query("APIC")) == NULL) {
         panic("no acpi madt detected\n");
@@ -35,6 +98,35 @@ hal_irqchip_init(void)
         pr_trace("dual i8259 @ mainboard; disabling for apic\n");
         md_outb(0xA1, 0xFF);
         md_outb(0x21, 0xFF);
+    }
+
+    cur = (uint8_t *)(madt + 1);
+    end = (uint8_t *)madt + madt->hdr.length;
+    while (cur < end) {
+        apichdr = (void *)cur;
+
+        switch (apichdr->type) {
+        case IRQCHIP_LAPIC:
+            lapic = (struct local_apic *)apichdr;
+            chip.base_addr = madt->lapic_addr;
+            chip.gsi_base = 0;
+            chip.id = lapic->apic_id;
+            chip.type = IRQCHIP_LAPIC;
+            lapic_add(&chip, lapic->processor_id);
+            break;
+        case IRQCHIP_IOAPIC:
+            ioapic = (struct ioapic *)apichdr;
+            chip.type = IRQCHIP_IOAPIC;
+            chip.id = ioapic->ioapic_id;
+            chip.gsi_base = ioapic->gsi_base;
+            chip.base_addr = ioapic->ioapic_addr;
+            chip.flags = 0;
+            ioapic_add(&chip);
+            break;
+
+        }
+
+        cur += apichdr->length;
     }
 
     return 0;
--- src/m1x/include/hal/irqchip.h
+++ src/m1x/include/hal/irqchip.h
@@ -8,9 +8,57 @@
 
 #include <sys/types.h>
 
+#define IRQCHIP_MAX 32
+
+/*
+ * Represents the various kinds of IRQ chips
+ *
+ * @IRQCHIP_LAPIC: This is a Local APIC unit
+ * @IRQCHIP_IOAPIC: This is an I/O APIC unit
+ */
+typedef enum {
+    IRQCHIP_LAPIC,
+    IRQCHIP_IOAPIC
+} irqchip_type_t;
+
+/*
+ * Describes a platform IRQ chip
+ *
+ * @type:       IRQ chip type
+ * @id:         IRQ chip ID (assigned by hardware)
+ * @gsi_base:   Applies to ACPI and I/O APICs
+ * @flags:      Chip specific flags
+ * @base_addr:  MMIO base address
+ */
+struct irqchip {
+    irqchip_type_t type;
+    uint8_t id;
+    uint8_t gsi_base;
+    uint32_t flags;
+    uintptr_t base_addr;
+};
+
 /*
  * Initialize the IRQ chip manager
  */
 int hal_irqchip_init(void);
+
+/*
+ * Obtain an I/O APIC by index
+ *
+ * @index: Index of I/O APIC to obtain
+ *
+ * Returns NULL if not found
+ */
+struct irqchip *md_ioapic_index(size_t index);
+
+/*
+ * Obtain an Local APIC by index
+ *
+ * @index: Index of Local APIC to obtain
+ *
+ * Returns NULL if not found
+ */
+struct irqchip *md_lapic_index(size_t index);
 
 #endif  /* !_HAL_IRQCHIP_H_ */
