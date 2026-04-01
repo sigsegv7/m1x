9cf32d2e1eb24ef26cbe1914dbaa924f27b4396d
Ian Moffett <ian@mirocom.org>
2026-03-31 22:06:37 -0400

    m1x/x86_64: Add C-side GDT definitions
    Signed-off-by: Ian Moffett <ian@mirocom.org>

--- src/m1x/include/arch/x86_64/gdt.h
+++ src/m1x/include/arch/x86_64/gdt.h
@@ -6,6 +6,11 @@
 #ifndef _MACHINE_GDT_H_
 #define _MACHINE_GDT_H_ 1
 
+#ifndef __ASSEMBLER__
+#include <sys/types.h>
+#include <sys/cdefs.h>
+#endif
+
 /* Kernel code/data */
 #define GDT_KCODE  0x08
 #define GDT_KDATA  0x10
@@ -18,4 +23,20 @@
 #define GDT_TSS    0x28
 #define GDT_TSS_INDEX 0x05
 
+#ifndef __ASSEMBLER__
+struct __packed gdt_entry {
+    uint16_t limit;
+    uint16_t base_low;
+    uint8_t base_mid;
+    uint16_t attributes;
+    uint8_t base_hi;
+};
+
+struct __packed gdtr {
+    uint16_t limit;
+    uintptr_t offset;
+};
+
+extern struct gdt_entry g_GDT[];
+#endif
 #endif  /* !_MACHINE_GDT_H_ */
