79a6c4b63400f0450f91e0ff22248d30da43319c
Ian Moffett <ian@mirocom.org>
2026-04-02 01:18:50 -0400

    sdk: sys: Add initial sys/limits.h header

---
+++ src/sdk/include/sys/limits.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026, Mirocom Laboratories
+ * Provided under the BSD-3 clause
+ */
+
+#ifndef _SYS_LIMITS_H_
+#define _SYS_LIMITS_H_ 1
+
+/* Maximum file name length */
+#define NAME_MAX 128
+
+#endif /* !_SYS_LIMITS_H_ */
